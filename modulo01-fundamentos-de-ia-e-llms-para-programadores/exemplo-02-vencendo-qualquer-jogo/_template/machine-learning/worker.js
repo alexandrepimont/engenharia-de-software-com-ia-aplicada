a5a94889e0b91448728409464047b406f9f7d02e
Alexandre Pimont Penha <alexandrepenha.geologia@gmail.com>
2026-03-21 09:34:01 +0000

    continue dev

--- modulo01-fundamentos-de-ia-e-llms-para-programadores/exemplo-02-vencendo-qualquer-jogo/_template/machine-learning/worker.js
+++ modulo01-fundamentos-de-ia-e-llms-para-programadores/exemplo-02-vencendo-qualquer-jogo/_template/machine-learning/worker.js
@@ -3,6 +3,7 @@ importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
 const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
 const INPUT_MODEL_DIMENTIONS = 640
+const CLASS_THRESHOLD = 0.4
 
 let _labels = []
 let _model = null
@@ -51,6 +52,37 @@ async function runInference(tensor) {
         classes: classesData
     }
 }
+
+// funcao geradora, pra cada item
+function   * processPrediction({boxes, scores, classes}, width, height) {
+    for (let index = 0; index < scores.length; index++) {
+        if (scores[index] < CLASS_THRESHOLD) continue
+
+        const label = _labels[classes[index]]
+        if (label !== 'kite') continue
+
+        let [x1, y1, x2, y2] = boxes.slice(index * 4, (index + 1) * 4)
+
+        x1 = x1 * width
+        y1 = y1 * height
+        x2 = x2 * width
+        y2 = y2 * height
+
+        
+        const boxWidth = x2 - x1
+        const boxHeight = y2 - y1
+
+        const centerX = x1 + boxWidth / 2
+        const centerY = y1 + boxHeight / 2
+
+        yield {
+            x: centerX,
+            y: centerY,
+            score: (scores[index] * 100).toFixed(2)
+        }
+    }
+}
+
 loadModelAndLabels()
 
 
@@ -62,14 +94,20 @@ self.onmessage = async ({ data }) => {
     const {width, height} = data.image
     
     const inferenceResults = await runInference(input)
-    debugger
+
+    for (const prediction of processPrediction(inferenceResults, width, height)) {
+        postMessage({
+            type: 'prediction',
+            ...prediction
+        });
+    }
     
-    postMessage({
-        type: 'prediction',
-        x: 400,
-        y: 400,
-        score: 0
-    });
+    // postMessage({
+    //     type: 'prediction',
+    //     x: 400,
+    //     y: 400,
+    //     score: 0
+    // });
 
 
 };
